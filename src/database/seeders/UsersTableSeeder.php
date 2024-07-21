a5aa1862977c29d41eef83cc4aed8931fcfd1552
tatsuhiro <T.Hayashi4218538@gmail.com>
2024-07-21 11:17:20 +0900

    20240721

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,31 +19,36 @@ class UsersTableSeeder extends Seeder
         $param = [
             'name' => 'test1',
             'email' => 'test@example1.com',
-            'password' => 'testtesttest',
+            'password' => Hash::make('testtesttest'),
+            'remember_token' => Str::random(10),
         ];
         DB::table('users')->insert($param);
         $param = [
             'name' => 'test2',
             'email' => 'test@example2.com',
-            'password' => 'testtesttest',
+            'password' => Hash::make('testtesttest'),
+            'remember_token' => Str::random(10),
         ];
         DB::table('users')->insert($param);
         $param = [
             'name' => 'test3',
             'email' => 'test@example3.com',
-            'password' => 'testtesttest',
+            'password' => Hash::make('testtesttest'),
+            'remember_token' => Str::random(10),
         ];
         DB::table('users')->insert($param);
         $param = [
             'name' => 'test4',
             'email' => 'test@example4.com',
-            'password' => 'testtesttest',
+            'password' => Hash::make('testtesttest'),
+            'remember_token' => Str::random(10),
         ];
         DB::table('users')->insert($param);
         $param = [
             'name' => 'test5',
             'email' => 'test@example5.com',
-            'password' => 'testtesttest',
+            'password' => Hash::make('testtesttest'),
+            'remember_token' => Str::random(10),
         ];
         DB::table('users')->insert($param);
     }
